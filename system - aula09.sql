0b1949bce001405d56d0027d9b4bfd5a2c52a80e
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-10-14 08:38:18 -0300

    Update system - aula09.sql

--- system - aula09.sql
+++ system - aula09.sql
@@ -3164,7 +3164,17 @@ SELECT *
 FROM atleta
 WHERE id = 188;
 
-
+CREATE TABLE audit_atleta_sal
+(
+    id_atleta  NUMBER(4),
+    sal_antigo NUMBER(8, 2),
+    sal_novo   NUMBER(8, 2),
+    usuario    VARCHAR2(20),
+    data_alt   TIMESTAMP,
+    CONSTRAINT audit_atl_sal_pk PRIMARY KEY (id_atleta, data_alt),
+    CONSTRAINT audit_atl_sal_id_fk FOREIGN KEY (id_atleta)
+        REFERENCES atleta (id)
+)
 
 
 
